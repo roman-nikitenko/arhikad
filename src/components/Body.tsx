b977f700d584bddf3f1bd2f109e8e452f7f121ce
Roman Nikitenko <Lomkaaa@ukr.net>
2024-05-22 21:46:19 +0300

    fix

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import plan from '../assets/plan3.jpg';
 import plan2 from '../assets/plan2.jpg'
 import { Button } from './Button.tsx';
@@ -13,18 +13,22 @@ export const Body: React.FC = () => {
   const { ref: madeSection, inView } = useInView();
   const { ref: aboutSection, inView: inViewAbout } = useInView();
   
+  const aboutSectionRef = useRef<HTMLDivElement>(null);
+  // const servicesSectionRef = useRef<HTMLDivElement>(null);
+  // const projectsSectionRef = useRef<HTMLDivElement>(null);
+  
   
   return (
     <div className="max-w-screen-2xl m-auto flex flex-col items-center gap-y-[100px]">
       <section ref={aboutSection} className="section-md">
         <div className="h-[440px] relative ">
-          <img src={plan} className="w-full h-full" alt='plan image'/>
+          <img src={plan} className="w-full z-0 h-full" alt='plan image'/>
           <div className={`absolute top-0 left-0 size-[130px] border border-[var(----accent-color)] -z-10 transform ${inViewAbout ? '-translate-x-[10px] -translate-y-[10px]' : ''} transition duration-700`} />
-          <div className={`absolute z-10  top-0 w-0 h-0 transform -translate-x-full -translate-y-full transition duration-700 ${inViewAbout ? '-translate-x-0 -translate-y-0' : ''}
+          <div className={`absolute z-10  top-0 w-0 h-0 transform -translate-x-full -translate-y-full transition duration-700 ${inViewAbout ? 'translate-x-0 translate-y-0' : ''}
           border-t-[55px] border-t-white
           border-r-[55px] border-r-transparent`}/>
         </div>
-        <div className="w-[500px] flex flex-col gap-7 justify-center">
+        <div ref={aboutSectionRef} className="w-[500px] flex flex-col gap-7 justify-center">
           <p className="text-emerald-700 uppercase">/about us</p>
           <h2 className="text-3xl font-bold">A group of reliability and expertise contractors</h2>
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque consequatur corporis deleniti dicta dolor dolorem doloribus ducimus earum est ex explicabo fuga, impedit ipsum, nostrum officiis tempora unde vel voluptatum?</p>
@@ -75,7 +79,7 @@ export const Body: React.FC = () => {
               <div className="absolute size-full bg-gradient-to-t from-black opacity-70 to-transparent" />
               <p className="title text-white text-3xl ml-6 mb-10 z-10 font-normal">Residential Construction</p>
             </div>
-            <div className={`bg-[url("assets/develop2.avif")] relative bg-cover size-full flex items-end`} >
+            <div className={`bg-[url("assets/develop2.jpg")] relative bg-cover size-full flex items-end`} >
               <div className="absolute size-full bg-gradient-to-t from-black opacity-70 to-transparent" />
               <p className="title text-white text-3xl ml-6 mb-10 z-10 font-normal">Interior Design</p>
             </div>
