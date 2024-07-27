4f68bf50d828dadd64bf5592eb49eb2a5e4b723e
Roman Nikitenko <Lomkaaa@ukr.net>
2024-07-27 14:34:06 +0300

    fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,14 @@
 import { Header } from './components/Header.tsx';
 import { Body } from './components/Body.tsx';
 import { Footer } from './components/Footer.tsx';
-import { useRef } from 'react';
 import { CallMe } from './components/CallMe.tsx';
 
 function App() {
-  const aboutSectionRef = useRef<HTMLDivElement>(null);
-  const servicesSectionRef = useRef<HTMLDivElement>(null);
-  const projectsSectionRef = useRef<HTMLDivElement>(null);
   
   return (
     <>
       <CallMe />
-      <Header aboutSectionRef={aboutSectionRef} servicesSectionRef={servicesSectionRef} projectsSectionRef={projectsSectionRef} />
+      <Header />
       <Body />
       <Footer/>
     </>
